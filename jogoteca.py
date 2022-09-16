930d6ce88a1cfb952ea6964575b681d87345d1d5
samuel-sanp <samuel.junior@sanp.com.br>
2022-09-16 11:27:22 -0300

    redirect new page for home.

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 class Game:
@@ -12,13 +12,14 @@ app = Flask(__name__)
 host = '0.0.0.0'
 port = 8080
 
+game1 = Game('Game 1', 'Category 1', 'Console 1')
+game2 = Game('Game 2', 'Category 2', 'Console 2')
+game3 = Game('Game 3', 'Category 3', 'Console 3')
+games = [game1, game2, game3]
+
 
 @app.route('/')
-def home():
-    game1 = Game('Game 1', 'Category 1', 'Console 1')
-    game2 = Game('Game 2', 'Category 2', 'Console 2')
-    game3 = Game('Game 3', 'Category 3', 'Console 3')
-    games = [game1, game2, game3]
+def index():
     return render_template('list.html', title="Jogos", games=games)
 
 
@@ -27,4 +28,16 @@ def new():
     return render_template('new.html', title="Novo Jogo")
 
 
-app.run(host, port)
+@app.route('/create', methods=['POST', ])
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+
+    new_game = Game(name, category, console)
+    games.append(new_game)
+
+    return redirect('/')
+
+
+app.run(host, port, debug=True)
