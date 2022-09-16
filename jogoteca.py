a923d8a507c7e96f6edf144e1718ccadf34808ab
samuel-sanp <samuel.junior@sanp.com.br>
2022-09-16 10:20:52 -0300

    add interator for.

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+host = '0.0.0.0'
+port = 8080
+
 
 @app.route('/')
 def hello():
-    return render_template('list.html')
+    games = ['game 1', 'game 2', 'game 3']
+    return render_template('list.html', title="Jogos", games=games)
 
-app.run()
+
+app.run(host, port)
