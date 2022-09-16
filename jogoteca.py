344f33357c04ae7d1df30f0dfc6554a75d2fd797
samuel-sanp <samuel.junior@sanp.com.br>
2022-09-16 10:52:55 -0300

    add form new game

--- jogoteca.py
+++ jogoteca.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
 
+
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
 app = Flask(__name__)
 host = '0.0.0.0'
 port = 8080
 
 
 @app.route('/')
-def hello():
-    games = ['game 1', 'game 2', 'game 3']
+def home():
+    game1 = Game('Game 1', 'Category 1', 'Console 1')
+    game2 = Game('Game 2', 'Category 2', 'Console 2')
+    game3 = Game('Game 3', 'Category 3', 'Console 3')
+    games = [game1, game2, game3]
     return render_template('list.html', title="Jogos", games=games)
+
+
+@app.route('/new')
+def new():
+    return render_template('new.html', title="Novo Jogo")
 
 
 app.run(host, port)
