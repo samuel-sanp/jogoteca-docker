9a607484cbbcaf9ca80e74d22c03ad31228daf85
samuel-sanp <samuel.junior@sanp.com.br>
2022-09-16 15:13:44 -0300

    refact login .

--- jogoteca.py
+++ jogoteca.py
@@ -8,15 +8,33 @@ class Game:
         self.console = console
 
 
-app = Flask(__name__)
-app.secret_key = 'cookie_secret_key'
-host = '0.0.0.0'
-port = 8080
+class User:
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
+
+
+user1 = User('Samuel Constantino', 'samuel', '123')
+user2 = User('User One', 'user1', '123')
+user3 = User('User Two', 'user2', '123')
+
+users = {
+    user1.username: user1,
+    user2.username: user2,
+    user3.username: user3,
+}
 
 game1 = Game('Game 1', 'Category 1', 'Console 1')
 game2 = Game('Game 2', 'Category 2', 'Console 2')
 game3 = Game('Game 3', 'Category 3', 'Console 3')
+
 games = [game1, game2, game3]
+
+app = Flask(__name__)
+app.secret_key = 'cookie_secret_key'
+host = '0.0.0.0'
+port = 8080
 
 
 @app.route('/')
@@ -55,13 +73,15 @@ def auth():
     password = request.form['password']
     next_page = request.form['next_page']
 
-    if username == 'user' and password == '123':
-        session['user_jogoteca'] = username
-        flash(username + ' logado com sucesso')
-        return redirect(next_page)
-    else:
-        flash('logado não logado')
-        return redirect(url_for('login'))
+    if username in users:
+        if password == users[username].password:
+            session['user_jogoteca'] = username
+            flash(users[username].name + ' logado com sucesso')
+            return redirect(next_page)
+
+    flash('logado não logado')
+    return redirect(url_for('login'))
+
 
 
 @app.route('/logout')
