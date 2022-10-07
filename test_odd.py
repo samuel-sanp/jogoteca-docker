0392e261e7dc146ccfbef5f49b3c2a46c56e5c39
samuel-sanp <samuel.junior@sanp.com.br>
2022-10-07 12:08:34 -0300

    add fail test.

--- test_odd.py
+++ test_odd.py
@@ -3,7 +3,7 @@ from odd import is_odd
 
 def test_is_odd_when_number_is_odd_returns_true():
     'Para um número ímpar a função deve retornar o valor True'
-    assert is_odd(3) is False
+    assert is_odd(5) is False
 
 
 def test_is_odd_when_number_is_even_returns_false():
